1d67ff5eb8fef338a531986646bc4871a37e060e
Kseniia <klimentova.k@ati.su>
2021-06-06 15:50:07 +0300

    change func name

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_basket_button_exists(browser):
     browser.get(link)
     time.sleep(30)
     assert browser.find_element_by_class_name("btn-add-to-basket") is not None, "корзинка не найдена"
