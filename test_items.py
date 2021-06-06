a981836b5dcc46231e0fd380a2a43c79d5bbf7a3
Kseniia <klimentova.k@ati.su>
2021-06-06 15:51:31 +0300

    change message

--- test_items.py
+++ test_items.py
@@ -4,4 +4,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_basket_button_exists(browser):
     browser.get(link)
     time.sleep(30)
-    assert browser.find_element_by_class_name("btn-add-to-basket") is not None, "корзинка не найдена"
+    assert browser.find_element_by_class_name("btn-add-to-basket") is not None, "Нет корзины"
